e532073a41d18f22576ef013dd22da5276c4c35c
avc0021 <avc0021@gmail.com>
2021-11-11 19:58:18 -0600

    js correction

--- assets/js/game.js
+++ assets/js/game.js
@@ -81,10 +81,7 @@ for (var i = 0; i < enemyNames.length; i++) {
 
     // reset enemyHealth before starting new fight
     enemyHealth = 50;
-
-    // use debugger to pause script from running and check what's going on at that moment in the code
-    debugger;
-
+    
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
   }
